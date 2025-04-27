f6c75525200d2dd1cf6c1e55582c332ab0acc9ab
jmolenaa <jmolenaa@student.codam.nl>
2025-04-27 17:48:01 +0200

    in progress

--- Makefile
+++ Makefile
@@ -1,13 +1,54 @@
+RED=\033[1;31m
+GREEN=\033[1;32m
+YELLOW=\033[1;33m
+BLUE=\033[1;34m
+MAGENTA=\033[1;35m
+CYAN=\033[1;36m
+END=\033[0m
+
 BACKEND := ./project/backend
 FRONTEND := ./project/frontend
 
+NODE_VERSION := v22.2
+
+# scripts
+NODE_INIT = ./init_files/node_init.sh
+
+
+
+# TODO
+# for later setup a production branch
+# and a frontend setup
 # build stuff here
-all:
+all: dev
+
+
+production:
+	@echo "${RED}No production flow yet :(${END}"
+
+
+# startup server
+dev: node backend frontend
+	@echo huh
+
+
+node:
+	@export NODE_VERSION=${NODE_VERSION} && ${NODE_INIT}
+
+backend:
+
+
+
+frontend:
+	@echo "${YELLOW}Frontend doesn't require building yet${END}"
+
+
+package:
+	echo hi
 
 
 # initiliase, so npm init and install all the various packages we need
 init:
-
 
 
 
@@ -20,8 +61,6 @@ fclean: clean
 
 
 re: fclean init
-
-
 
 
 .PHONY: all init clean re fclean
---
+++ init_files/node_init.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+MAGENTA='\033[1;35m'
+CYAN='\033[1;36m'
+END='\033[0m'
+
+# this line is here to load nvm command into the shell if it is already installed
+# if this isn't here the script will always flag nvm as uninstalled and try to install it
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# checking if we have node installed and if so if it's the correct version
+if ! command -v node > /dev/null ; then 
+    NEED_NODE_INSTALL=true
+else
+    CURRENT_NODE_VERSION=$(node -v | cut -d'.' -f1-2)
+    if [ "$CURRENT_NODE_VERSION" != "$NODE_VERSION" ]; then
+        NEED_NODE_INSTALL=true
+    else
+        NEED_NODE_INSTALL=false
+    fi
+fi
+
+# checking if we have nvm installed
+if ! command -v nvm > /dev/null ; then
+    NEED_NVM_INSTALL=true
+else
+    NEED_NVM_INSTALL=false
+fi
+
+# based on what has been installed, install node and nvm
+if [ "$NEED_NODE_INSTALL" = true ]; then
+
+    if [ "$NEED_NVM_INSTALL" = true ]; then
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    fi
+    nvm install $NODE_VERSION
+    nvm use $NODE_VERSION
+    echo -e "$GREEN Node has been installed\
+            Before using, please source your shell, either by turning it off and on"
+fi
+
+echo "$GREEN Node is "
