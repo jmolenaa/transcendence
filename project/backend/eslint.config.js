7ce23eabf8dd665f892750aaf229674db6511b5f
jmolenaa <jmolenaa@student.codam.nl>
2025-05-04 17:37:26 +0200

    added configuration files for eslint, prettier, added them to the json files and added rules for running eslint, havent ran it on the files yet.

--- project/backend/eslint.config.js
+++ project/backend/eslint.config.js
@@ -1,11 +1,39 @@
 import js from "@eslint/js";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
+  {
+    // Specifies the files that this configuration will apply to, in this case all Javascript
+    files: ["**/*.{js,mjs,cjs}"],
+
+    // Configures language options, such as global variables for the environment [1]
+    languageOptions: {
+      globals: globals.node,
+    },
+
+    // Specifies plugins to enhance ESLint functionality, in this case, the JavaScript plugin and Prettier plugin
+    plugins: {
+      js,
+      prettier: prettierPlugin,
+    },
+
+    // Extends the recommended ESLint rules for JavaScript
+    extends: [
+      "js/recommended"
+    ],
+
+    // Defines custom rules for the project, if this isn't here the prettier errors will not show up, nor will the code be formatted
+    // TODO enforce it
+    // not enforced for now, so not to many diffs when merging
+
+    // rules: {
+    //   "prettier/prettier": "error", // Enforces Prettier formatting as an error
+    // },
+  },
 ]);
+
+/*
+[1] This is important because Node.js has its own set of global variables and functions that are not available in the browser. By specifying the Node.js environment, ESLint will recognize these globals and avoid flagging them as undefined.
+*/
---
+++ project/backend/prettier.config.js
@@ -0,0 +1,7 @@
+export default {
+    semi: true,              // Add semicolons
+    singleQuote: true,       // Use single quotes instead of double
+    tabWidth: 2,             // 2 spaces per tab
+    useTabs: false,          // Use spaces instead of tabs
+    printWidth: 100,         // Line length limit
+  };
