0268b18b5396bc3317a84c204c6bb02c62e5388e
jmolenaa <jmolenaa@student.codam.nl>
2025-05-14 14:25:44 +0200

    initialised frontend, added packages for linting and configured them

---
+++ project/frontend/eslint.config.mjs
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import css from "@eslint/css";
+import prettierPlugin from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs,ts}"], 
+    languageOptions: { 
+      parser: tseslint.parser,
+      globals: {...globals.browser, ...globals.node} 
+    },
+    
+    plugins: { 
+      js,
+      prettier: prettierPlugin,
+      '@typescript-eslint': tseslint.plugin,
+    }, 
+    
+    extends: [
+      "js/recommended",
+      "@typescript-eslint/recommended",
+    ],
+    rules: {
+      "prettier/prettier": "error",
+    },
+  
+  },
+
+  tseslint.configs.recommended,
+  { 
+    files: ["**/*.css"], 
+    plugins: { css }, 
+    language: "css/css", 
+    extends: ["css/recommended"] 
+  },
+]);
